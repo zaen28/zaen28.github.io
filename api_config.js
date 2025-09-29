d827b320cf5eed0d4206277ae3de67779d37e0b9
zaen28 <z01634057@gmail.com>
2025-09-30 04:55:45 +0700

    Create api_config.js

---
+++ api_config.js
@@ -0,0 +1,4 @@
+const YOUTUBE_API_ENDPOINT = 'https://api.deline.my.id/downloader/ytplay';
+const TIKTOK_API_ENDPOINT  = 'https://api.deline.my.id/downloader/tiktok';
+const PINTEREST_API_ENDPOINT = 'https://api.deline.my.id/downloader/pinterest';
+const CAPCUT_API_ENDPOINT    = 'https://api.deline.my.id/downloader/capcut';
